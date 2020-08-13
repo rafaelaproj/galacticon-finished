b923e9102333421a137e358d4de00d18f687b15d
rafaelaproj <rafaelaproj@gmail.com>
2020-08-13 17:54:49 -0300

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/galacticon/MainActivity.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.galacticon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.*
+
+class MainActivity : AppCompatActivity(), ImageRequester.ImageRequesterResponse {
+
+  private lateinit var gridLayoutManager: GridLayoutManager
+  private var photosList: ArrayList<Photo> = ArrayList()
+  private lateinit var adapter: RecyclerAdapter
+  private lateinit var imageRequester: ImageRequester
+  private lateinit var linearLayoutManager: LinearLayoutManager
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    linearLayoutManager = LinearLayoutManager(this)
+    recyclerView.layoutManager = linearLayoutManager
+    gridLayoutManager = GridLayoutManager(this, 2)
+    adapter = RecyclerAdapter(photosList)
+    recyclerView.adapter = adapter
+    setRecyclerViewScrollListener()
+    setRecyclerViewItemTouchListener()
+
+    imageRequester = ImageRequester(this)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    if(photosList.size == 0) {
+      requestPhoto()
+    }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if(item.itemId == R.id.action_change_recycler_manager) {
+      changeLayoutManager()
+      return true
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun requestPhoto() {
+    try {
+      imageRequester.getPhoto()
+    } catch (e: IOException) {
+      e.printStackTrace()
+    }
+
+  }
+
+  override fun receivedNewPhoto(newPhoto: Photo) {
+    runOnUiThread {
+      photosList.add(newPhoto)
+      adapter.notifyItemInserted(photosList.size-1)
+    }
+  }
+
+  private val lastVisibleItemPosition: Int
+    get() = if(recyclerView.layoutManager == linearLayoutManager) {
+      linearLayoutManager.findLastVisibleItemPosition()
+    } else {
+      gridLayoutManager.findLastVisibleItemPosition()
+    }
+
+  private fun setRecyclerViewScrollListener() {
+    recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+      override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
+        val totalItemCount = recyclerView.layoutManager!!.itemCount
+        if (!imageRequester.isLoadingData && totalItemCount == lastVisibleItemPosition + 1) {
+          requestPhoto()
+        }
+      }
+    })
+  }
+
+  private fun setRecyclerViewItemTouchListener() {
+    val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+      override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                          target: RecyclerView.ViewHolder): Boolean {
+        return false
+      }
+
+      override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
+        val position = viewHolder.adapterPosition
+        photosList.removeAt(position)
+        recyclerView.adapter!!.notifyItemRemoved(position)
+      }
+    }
+
+    val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
+    itemTouchHelper.attachToRecyclerView(recyclerView)
+  }
+
+  private fun changeLayoutManager() {
+    if(recyclerView.layoutManager == linearLayoutManager) {
+      recyclerView.layoutManager = gridLayoutManager
+      if(photosList.size == 1) {
+        requestPhoto()
+      }
+    } else {
+      recyclerView.layoutManager = linearLayoutManager
+    }
+  }
+}
